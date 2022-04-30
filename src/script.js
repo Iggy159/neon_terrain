3037634906fd4f333c026916bf753b170d50e007
Iggy159 <georgebazhenov27@gmail.com>
2022-04-30 13:13:00 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,109 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Cube
+ */
+const land = {}
+
+land.geometry = new THREE.PlaneGeometry(1, 1, 40, 40)
+land.geometry.rotateX(- Math.PI * 0.5)
+
+land.material = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    wireframe: true,
+    uniforms: {
+        uTime: { value: 0 }
+    }
+})
+
+land.mesh = new THREE.Mesh(land.geometry, land.material)
+
+scene.add(land.mesh)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let lastElapsedTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - lastElapsedTime
+    lastElapsedTime = elapsedTime
+
+    land.material.uniforms.uTime.value = elapsedTime
+    
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
---
+++ src/shaders/vertex.glsl
@@ -0,0 +1,44 @@
+#pragma glslify: cnoise3 = require(glsl-noise/classic/3d)
+
+varying float vElevation;
+uniform float uTime;
+uniform float uElevation;
+
+float getElevation(vec2 _position) {
+
+    float elevation = 0.0;
+
+    vec2 position = _position;
+    position.x -= uTime * 0.1;
+    //position.y += uTime * 0.1;
+
+    elevation = cnoise3(vec3(
+        (position * 10.) * 0.4,
+        0.0
+    )) * .08;
+
+    elevation = cnoise3(vec3(
+        (position * 40.) * 0.2,
+        0.0
+    )) * 0.09;
+
+    elevation += uElevation;
+
+    return elevation;
+}
+
+void main() {
+
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    float elevation = getElevation(modelPosition.xz);
+
+    modelPosition.y += elevation;
+    
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectionPosition;
+
+    vElevation = elevation;
+}
